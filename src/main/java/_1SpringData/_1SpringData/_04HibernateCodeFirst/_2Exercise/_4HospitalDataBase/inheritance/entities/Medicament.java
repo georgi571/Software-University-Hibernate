a080f51679516e2b2c11484563c7d8f14b14e38d
Georgi Kostadinov <georgi93571@gmail.com>
2024-09-24 20:52:36 +0300

    add solution

---
+++ src/main/java/_1SpringData/_1SpringData/_04HibernateCodeFirst/_2Exercise/_4HospitalDataBase/inheritance/entities/Medicament.java
@@ -0,0 +1,20 @@
+package _1SpringData._1SpringData._04HibernateCodeFirst._2Exercise._4HospitalDataBase.inheritance.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "medicaments")
+public class Medicament {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "medicaments")
+    private Set<Diagnose> diagnoses;
+}
---
+++ src/main/java/_1SpringData/_1SpringData/_04HibernateCodeFirst/_2Exercise/_4HospitalDataBase/inheritance/entities/Visitation.java
@@ -0,0 +1,28 @@
+package _1SpringData._1SpringData._04HibernateCodeFirst._2Exercise._4HospitalDataBase.inheritance.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "visitations")
+public class Visitation {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "patient_id", referencedColumnName = "id")
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "diagnose_id", referencedColumnName = "id")
+    private Diagnose diagnose;
+}
